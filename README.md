98c98608177b9eeda3ae0cc35faeb07cb64f29d8
Mark Lehmacher <Mark.Lehmacher+gh@gmail.com>
2012-02-11 14:34:11 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+Media Library
+=============
+
+A node.js application for managing media meta data and encapsulating access to the media stream for clients
+to play media, not caring whether it is stored on your local hard disk or in the cloud.
+
+The long term goal is to make this actually work for all kinds of media. My short term goal is driven by my
+particular use case which is "listening to music stored in mp3 files on my nas at home from within the HTML5
+browser at work".
+
+
+Status
+------
+
+*February 2012: Initial project. Not sensibly usable for anyone but me (in the current state not even for me).
+
+
+Architecture
+------------
+
+The architecture is a result of the following requirements:
+* Clean separation between player clients and the library which stores all the media meta data.
+* The player client must be agnostic of physical location of media. It does not matter if media is stored on
+my local hard drive or within the cloud.
+* Gather some experience with RESTful API design.
+* Gather some experience with server side JavaScript, in particular with respect asynchronous
+code following the node.js paradigm.
+* Gather some experience with developing a modern web application.
+
+
+``Library``: The library which is responsible for storing media meta data. Right now it is using couchdb for
+storage. The storage mechanism should also remain pluggable (unless that turns out to be too academic a
+premise).
+
+``Service``: The service on top of the library which exposes access to media meta data in a restful manner.
+The service also provides an API for streaming media. From the point of view of the client wanting to play
+media, the service is the facade in front of the physical media source (local hard disk, cloud, etc.).
+
+``Client (Player)``: The player uses the medialib service in order to browse/search media meta data as well
+as for streaming media.
+
+``Client (Cataloguer)``: The cataloguer uses the medialib service in order to add media meta data to the
+library and in order to register physical media data locations with the library.  
+
+The medialib service should be easily extensible in order to provide additional features in the future such as:
+* Server side cover art searching, maybe with Event-Channel into the Client over which events like "cover 
+found for album xyz, please show the user and let him approve or disapprove" can be send.
+* On the fly transcoding of media data.
+* Integration with last.fm or other services.
+
+
+Companion Projects
+------------------
+
+This project only contains the medialib server components, that is, the Library and the Service. The clients
+are contained in separate projects, which I will publish on github soon. Right now I am working on a browser
+player client (HTML5) and a scanning client for your local music. 
+
+
+Requirements
+------------
+* node v0.6.10
+* couchdb v1.0.2.0
+
+
+Utilities
+---------
+Also contains the following helpers, which may or may not be pulled out into separate modules in the future:
+* ``assert``: assertions for basic coding by contract
---
+++ spec/assert.spec.js
@@ -0,0 +1,116 @@
+var Assert = require( '../lib/medialib/util/assert' ).Assert;
+
+describe( 'Assert', function() {
+
+   // isNotNull
+   
+   it( 'throws error if null is required to be not null', function() {
+      expect( function() {
+         Assert.that( null ).isNotNull();
+      } ).toThrow( 'Assertion failed with cause: object is null' );
+   } );
+   
+   it( 'returns itself if undefined is required to be not null', function() {
+      expect( Assert.that( undefined ).isNotNull() ).toBeTruthy();
+   } );
+   
+   it( 'returns itself if an empty object is required to be not null', function() {
+      expect( Assert.that( {} ).isNotNull() ).toBeTruthy();
+   } );  
+   
+   // isNotUndefined
+   
+   it( 'throws error if undefined is required to be not undefined', function() {
+      expect( function() {
+         Assert.that( undefined ).isNotUndefined();
+      } ).toThrow( 'Assertion failed with cause: object is undefined' );
+   } );
+
+   it( 'returns itself if null is required to be not undefined', function() {
+      expect( Assert.that( null ).isNotUndefined() ).toBeTruthy();
+   } );
+   
+   // hasType
+   
+   it( 'throws error if undefined is required to have type String', function() {
+      expect( function() {
+         Assert.that( undefined ).hasType( String );
+      } ).toThrow( "Assertion failed with cause: object 'undefined' not of expected type 'String'" );
+   } );
+
+   it( 'throws error if null is required to have type String', function() {
+      expect( function() {
+         Assert.that( null ).hasType( String );
+      } ).toThrow( "Assertion failed with cause: object 'null' not of expected type 'String'" );
+   } );
+   
+   it( 'returns itself if {} is required to have type Object', function() {
+      expect( Assert.that( {} ).hasType( Object ) ).toBeTruthy();
+   } );
+   
+   it( 'returns itself if [] is required to have type Array', function() {
+      expect( Assert.that( [] ).hasType( Array ) ).toBeTruthy();
+   } );
+   
+   it( 'returns itself if "" (primitive) is required to have type String', function() {
+      expect( Assert.that( '' ).hasType( String ) ).toBeTruthy();
+   } );
+   
+   it( 'returns itself if "" (object) is required to have type String', function() {
+      expect( Assert.that( new String( '' ) ).hasType( String ) ).toBeTruthy();
+   } );
+   
+   it( 'returns itself if 0 (primitive) is required to have type Number', function() {
+      expect( Assert.that( 0 ).hasType( Number ) ).toBeTruthy();
+   } );
+
+   it( 'returns itself if false (primitive) is required to have type Boolean', function() {
+      expect( Assert.that( false ).hasType( Boolean ) ).toBeTruthy();
+   } );
+   
+   // hasKeys
+   
+   it( 'throws error if null is required to have key "a"', function() {
+      expect( function() {
+         Assert.that( null ).hasKeys( 'a' );
+      } ).toThrow( "Assertion failed with cause: object 'null' is missing keys: 'a'" );
+   } );
+   
+   it( 'throws error if null is required to have key "a"', function() {
+      expect( function() {
+         Assert.that( undefined ).hasKeys( 'a' );
+      } ).toThrow( "Assertion failed with cause: object 'undefined' is missing keys: 'a'" );
+   } );
+   
+   it( 'throws error if {} is required to have keys "a", "b", "c"', function() {
+      expect( function() {
+         Assert.that( {} ).hasKeys( 'a', 'b', 'c' );
+      } ).toThrow( "Assertion failed with cause: object '{}' is missing keys: 'a', 'b', 'c'" );
+   } );
+   
+   it( 'throws error if { a: null, b: null } is required to have keys "a", "b", "c"', function() {
+      expect( function() {
+         Assert.that( { a: null, b: null } ).hasKeys( 'a', 'b', 'c' );
+      } ).toThrow( "Assertion failed with cause: object '{\"a\":null,\"b\":null}' is missing keys: 'c'" );
+   } );
+   
+   it( 'returns itself if { a: null, b: null } is required to have keys "a", "b", specified as varargs', function() {
+      expect( Assert.that( { a: null, b: null } ).hasKeys( 'a', 'b' ) ).toBeTruthy();
+   } );
+   
+   it( 'returns itself if { a: null, b: null } is required to have keys "a", "b", specified as array', function() {
+      expect( Assert.that( { a: null, b: null } ).hasKeys( ['a', 'b'] ) ).toBeTruthy();
+   } );
+   
+   it( 'returns itself if { a: null, b: null } is required to have keys "a", "b", specified as object', function() {
+      expect( Assert.that( { a: null, b: null } ).hasKeys( { a: null, b: null } ) ).toBeTruthy();
+   } );
+   
+   // Chaining
+   
+   it( 'can be chained', function() {
+      expect( Assert.that( {} ).isNotUndefined().isNotNull() ).toBeTruthy();
+   } );
+
+
+} );
